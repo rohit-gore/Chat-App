a7ab50ba48ed5f2ab86e60ecdabb070303885a25
rohit-gore <679.nine@gmail.com>
2024-04-24 00:19:27 +0530

    user info changes

--- src/Components/list/userInfo/Userinfo.jsx
+++ src/Components/list/userInfo/Userinfo.jsx
@@ -2,7 +2,15 @@ import "./userinfo.css"
 
 const Userinfo = () => {
     return (
-        <div className="userInfo">Userinfo</div>
+        <div className="userInfo">
+            <div className="user"></div>
+
+            <div className="icons">
+                <img src="./more.png" alt="" />
+                <img src="" alt="" />
+                <img src="" alt="" />
+            </div>
+        </div>
     )
 }
 
