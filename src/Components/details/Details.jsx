6d427d23a923e7d2f39704075fc5f9718f60276c
rohit-gore <679.nine@gmail.com>
2024-05-13 19:38:38 +0530

    details section start

--- src/Components/details/Details.jsx
+++ src/Components/details/Details.jsx
@@ -2,7 +2,14 @@ import "./detail.css"
 
 const Detail = () => {
     return(
-        <div className="detail">Detail</div>
+        <div className="detail">
+            <div className="user">
+                <img src="./avatar.png" alt="" />
+                <h2>Jane Doe</h2>
+                
+            </div>
+            <div className="info"></div>
+        </div>
     )
 }
 
